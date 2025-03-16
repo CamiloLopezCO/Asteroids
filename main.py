ec530dd7489f59063a77cd7bec2b804f2fa80684
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-16 12:31:03 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#this allows us to use code from 
-#the open-source pygame library
-#throughout this file
+import pygame
 
-import pygame 
+#this allows us to use code from
+#the open-source pygame library
+#throughout this file 
+
+def main():
+	print("Starting Asteroids!")
 
 if __name__=="__main__":
 	main()

717b157b477edc409d50cb8b7cbaf9c1d255d10d
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-17 09:44:49 -0400

    Fixed main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def main():
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				return screen.fill("black")pygame.display.flip()
+				return 
+		screen.fill("black")
+		pygame.display.flip()
 
 if __name__=="__main__":
 	main()

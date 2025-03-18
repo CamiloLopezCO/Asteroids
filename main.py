f1e3470cb33021e0098ed6ffd824ebf19615fe7b
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-18 12:04:08 -0400

    Added asteroidfield.py & asteroid.py, also modified main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 #this allows us to use code from
 #the open-source pygame library
@@ -15,12 +16,17 @@ def main():
 	#Create groups
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 
-	#Set containers for the Player class
-	Player.containers = updatable, drawable 
+	#Set containers for the Player class and Asteroid
+	Player.containers = updatable, drawable
+	AsteroidField.containers = updatable
+	from asteroid import Asteroid
+	Asteroid.containers = asteroids, updatable, drawable
 
 	#Instantiate the player at the center of the screen
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
@@ -28,7 +34,7 @@ def main():
 				return
  
 		screen.fill("black") #Fill the screen with black
-		player.update(dt) #Update all updatables
+		updatable.update(dt) #Update all updatables
 		
 		#Draw all drawables
 		for sprite in drawable:

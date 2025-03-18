32a2d03691b56851f2778e56b7e344780de60012
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-18 12:53:54 -0400

    Added shot.py, modified constants.py, player.py & main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 from asteroidfield import AsteroidField
+from shot import Shot
 
 #this allows us to use code from
 #the open-source pygame library
@@ -17,12 +18,14 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	#Set containers for the Player class and Asteroid
 	Player.containers = updatable, drawable
 	AsteroidField.containers = updatable
 	from asteroid import Asteroid
 	Asteroid.containers = asteroids, updatable, drawable
+	Shot.containers = shots, updatable, drawable
 
 	#Instantiate the player at the center of the screen
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)

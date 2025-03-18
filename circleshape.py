a539eb8e20b275053627035a9b585215de1b43f6
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-18 00:33:06 -0400

    Fixed circleshape.py for indentation errors

--- circleshape.py
+++ circleshape.py
@@ -1,20 +1,20 @@
 import pygame
 
 class CircleShape(pygame.sprite.Sprite):
-	def __init__(self, x, y, radius):
-		#Automatically add to groups if "containers" exists
-		 if hasattr(self, "containers"):
-                        super().__init__(*self.containers)
-                else:
-                        super().__init__()
+  def __init__(self, x, y, radius):
+    #Automatically add to groups if "containers" exists
+    if hasattr(self, "containers"):
+      super().__init__(*self.containers)
+    else:
+      super().__init__()
 
-		self.position = pygame.Vector2(x, y)
-		self.velocity = pygame.Vector2(0, 0)
-		self.radius = radius
+    self.position = pygame.Vector2(x, y)
+    self.velocity = pygame.Vector2(0, 0)
+    self.radius = radius
 
-	def draw(self, screen):
-		pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+  def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
-	def update(self, dt):
-		#sub-classes must override
-		pass
+  def update(self, dt):
+  #sub-classes must override
+    pass

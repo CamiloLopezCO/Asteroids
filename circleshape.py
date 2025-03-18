e0b9f5871616d0b528c685d289e74349b32d2f78
CamiloLopezCO <camilolvh93@gmail.com>
2025-03-18 00:21:15 -0400

    Modified main.py & circleshape.py to add groups

--- circleshape.py
+++ circleshape.py
@@ -1,18 +1,16 @@
 import pygame
 
-#Base class for game objects 
 class CircleShape(pygame.sprite.Sprite):
 	def __init__(self, x, y, radius):
-		super().__init__()
+		#Automatically add to groups if "containers" exists
+		 if hasattr(self, "containers"):
+                        super().__init__(*self.containers)
+                else:
+                        super().__init__()
+
 		self.position = pygame.Vector2(x, y)
 		self.velocity = pygame.Vector2(0, 0)
 		self.radius = radius
-
-		#we will be using this later
-		#if hasattr(self, "containers"):
-			#super().__init__(self.containers)
-		#else:
-			#super().__init__()
 
 	def draw(self, screen):
 		pygame.draw.circle(screen, "white", self.position, self.radius, 2)
--- main.py
+++ main.py
@@ -9,10 +9,16 @@ from player import Player
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
 	clock = pygame.time.Clock()
 	dt = 0
 	
+	#Create groups
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	#Set containers for the Player class
+	Player.containers = updatable, drawable 
+
 	#Instantiate the player at the center of the screen
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -22,10 +28,13 @@ def main():
 				return
  
 		screen.fill("black") #Fill the screen with black
-		player.update(dt)
-		player.draw(screen) #Draw the player
-		pygame.display.flip() #Refresh the display
+		player.update(dt) #Update all updatables
+		
+		#Draw all drawables
+		for sprite in drawable:
+			sprite.draw(screen)
 
+		pygame.display.flip() #Refresh the display
 		dt = clock.tick(60) / 1000 #Limit to 60 FPS and calculate delta time
 
 if __name__=="__main__":
